1f3905683f0027769fa8f38702d0d32c8a8d1ba7
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-24 20:50:45 +0300

    getall and getbyid DONE

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,53 @@
-import bodyParser from "body-parser";
 import express from "express";
 import pg from "pg";
+require('dotenv').config({path: './.env'});
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
-const pool = new pg.Pool();
+const pool = new pg.Pool(
+  {
+    user: "postgres",
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: 6243
+  }
+);
+
+pool.connect();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
+app.use(express.raw({ type: "application/vnd.custom-type" }));
+app.use(express.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+app.get("/notes", async (req, res) => {
+  await pool.query("SELECT * FROM notes", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
 });
+
+app.get("/note/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  await pool.query("SELECT * FROM notes WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+
+  });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("*", (req, res) => {
+  res.send('no routes specified with this name!!!')
 });
