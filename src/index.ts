a0372967516c03d68d9e5de4f51a02122f977cb4
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-24 21:47:18 +0300

    crud DONE

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,52 @@ app.get("/note/:id", async (req, res) => {
     res.status(200).json(results.rows);
 
   });
-})
+});
+
+app.post("/note", (req, res) => {
+  
+  const { id, note, title } = req.body;
+
+  pool.query(
+    "INSERT INTO notes VALUES ($1, $2, $3)",
+    [id, note, title],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`Note added with ID: ${id}`);
+      console.log(results);
+    }
+  );
+});
+
+app.put("/note/:id", (req, res) => {
+  const { note, title } = req.body;
+  const id = parseInt(req.params.id);
+
+  pool.query(
+    "UPDATE notes SET note = $1, title = $2 WHERE id = $3",
+    [note, title, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`Note modified with ID: ${id}`);
+    }
+  );
+});
+
+app.delete("/note/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  // const { id } = req.body;
+
+  pool.query('DELETE FROM notes WHERE id = $1', [id], (error, results) => {
+      if (error) {
+          throw error
+      }
+      res.status(200).send(`Note deleted with ID: ${id}`)
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
